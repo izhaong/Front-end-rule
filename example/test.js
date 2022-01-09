15bd63ac17b76492239a0d8b3f13b079caaa7f9b
izhaong <izhaong@outlook.com>
2022-01-09 15:57:15 +0800

    feat: 增加测试文件

---
+++ example/test.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Date: 2022-01-09 15:46:07
+ * @LastEditTime: 2022-01-09 15:48:14
+ * @LastEditors: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Description:
+ * @FilePath: /tsl renren2 编码规范工具配置/example/test.js
+ */
+
+const aa = '123'
+
+function bb() {
+  console.log('bb')
+}
